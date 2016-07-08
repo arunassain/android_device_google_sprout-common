6c53a8f0ca5ed9cbd0e643f81f592d15aedc2d0f
mistercheese <quesoo@gmail.com>
2016-07-08 15:20:10 +0200

    sprout: add glove mode from sony's huashan

--- sprout.mk
+++ sprout.mk
@@ -28,6 +28,10 @@ PRODUCT_TAGS += dalvik.gc.type-precise
 # Dalvik heap
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
+# Glove mode
+PRODUCT_PACKAGES += \
+    DeviceSettings
+
 # Doze
 PRODUCT_PACKAGES += \
     Doze
